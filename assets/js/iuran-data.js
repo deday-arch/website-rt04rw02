2ba804822d60e5f4bfaf09b6810441b092dd0f74
deday-arch <deday-arch@users.noreply.github.com>
2026-07-23 10:37:24 +0700

    feat: tampilkan 235 data warga di iuran.html via iuran-data.js

---
+++ assets/js/iuran-data.js
@@ -0,0 +1,336 @@
+// ===== DATA WARGA IURAN - Card User List =====
+const WARGA = [
+  {no:'0000000001',nama:'ANGGI',hp:'089533007704'},
+  {no:'0000000002',nama:'EKA',hp:'081218443309'},
+  {no:'0000000003',nama:'LALA',hp:'-'},
+  {no:'0000000004',nama:'JAMAL',hp:'081384492047'},
+  {no:'0000000005',nama:'BERLIAN',hp:'-'},
+  {no:'0000000006',nama:'WAWANG',hp:'-'},
+  {no:'0000000007',nama:'WIWIK',hp:'-'},
+  {no:'0000000010',nama:'CASLORI',hp:'-'},
+  {no:'0000000017',nama:'ARIF / PARFUM',hp:'083117990602'},
+  {no:'0000000019',nama:'HJ ENCIS',hp:'-'},
+  {no:'0000000020',nama:'FAJAR',hp:'081212197900'},
+  {no:'0000000021',nama:'WIWID',hp:'-'},
+  {no:'0000000022',nama:'ASTOYADI',hp:'085811769417'},
+  {no:'0000000024',nama:'JAELANI',hp:'-'},
+  {no:'0000000026',nama:'JAUHARI',hp:'-'},
+  {no:'0000000028',nama:'HUSEN',hp:'081316233416'},
+  {no:'0000000029',nama:'SAMIYAN',hp:'-'},
+  {no:'0000000030',nama:'ZAINUDIN',hp:'-'},
+  {no:'0000000031',nama:'ILHAM KHOLIK',hp:'0857969727'},
+  {no:'0000000032',nama:'SOLIKHIN',hp:'089525510380'},
+  {no:'0000000033',nama:'ZAINUDIN',hp:'-'},
+  {no:'0000000034',nama:'BAGYO',hp:'081315744404'},
+  {no:'0000000036',nama:'AGUS',hp:'-'},
+  {no:'0000000037',nama:'MARITO',hp:'-'},
+  {no:'0000000038',nama:'DONI',hp:'081315447641'},
+  {no:'0000000039',nama:'PURWANTO',hp:'-'},
+  {no:'0000000052',nama:'INDOMART MARKET',hp:'-'},
+  {no:'0000000053',nama:'SIHOMBING',hp:'-'},
+  {no:'0000000054',nama:'CAFE DOOR',hp:'-'},
+  {no:'0000000055',nama:'EKA',hp:'081218443309'},
+  {no:'0000000056',nama:'SULTHAN',hp:'-'},
+  {no:'0000000057',nama:'DEDI KOSWARA',hp:'085778929037'},
+  {no:'0000000060',nama:'ANDI',hp:'081312631023'},
+  {no:'0000000061',nama:'EKO',hp:'087809228079'},
+  {no:'0000000067',nama:'EVAK',hp:'-'},
+  {no:'0000000068',nama:'SITI NGADIRAH',hp:'085199397756'},
+  {no:'0000000070',nama:'TRI SISWANTI',hp:'085715465538'},
+  {no:'0000000071',nama:'RUSDIAN',hp:'087866850986'},
+  {no:'0000000074',nama:'RULLY',hp:'085882912058'},
+  {no:'0000000075',nama:'HENI',hp:'-'},
+  {no:'0000000077',nama:'FERY H',hp:'085172365421'},
+  {no:'0000000078',nama:'LATIFAH',hp:'-'},
+  {no:'0000000088',nama:'CASLORI',hp:'-'},
+  {no:'0000000091',nama:'BABAN',hp:'085157106010'},
+  {no:'0000000107',nama:'AFIF',hp:'085885794230'},
+  {no:'0000000108',nama:'KOST AFIF',hp:'-'},
+  {no:'0000000109',nama:'KOST AFIF C',hp:'-'},
+  {no:'0000000111',nama:'AFUKARIM',hp:'085176755011'},
+  {no:'0000000112',nama:'YANI / BIMA',hp:'-'},
+  {no:'0000000113',nama:'YANI-BIMA',hp:'081277191679'},
+];
+const WARGA2 = [
+  {no:'0000000114',nama:'UYUM',hp:'-'},
+  {no:'0000000115',nama:'KADRAH',hp:'081382035494'},
+  {no:'0000000116',nama:'SADIMO',hp:'085732364760'},
+  {no:'0000000117',nama:'ATUN',hp:'-'},
+  {no:'0000000122',nama:'JENY',hp:'089601556933'},
+  {no:'0000000124',nama:'SARBINI',hp:'085881793115'},
+  {no:'0000000128',nama:'NDRI',hp:'-'},
+  {no:'0000000136',nama:'BASROWI',hp:'-'},
+  {no:'0000000144',nama:'KOST 1',hp:'-'},
+  {no:'0000000145',nama:'KOST 2',hp:'-'},
+  {no:'0000000146',nama:'KOST 3',hp:'-'},
+  {no:'0000000148',nama:'GUDANG JEEP',hp:'-'},
+  {no:'0000000149',nama:'RUMAH KSG',hp:'-'},
+  {no:'0000000160',nama:'KOST KIMBOK',hp:'-'},
+  {no:'0000000161',nama:'IRHAMUDIN',hp:'085715899610'},
+  {no:'0000000162',nama:'JUMANI',hp:'-'},
+  {no:'0000000163',nama:'SULISTYO',hp:'083878042606'},
+  {no:'0000000164',nama:'AJI SUBEKTI',hp:'-'},
+  {no:'0000000165',nama:'MULYANI',hp:'-'},
+  {no:'0000000166',nama:'YOSEF',hp:'081398623460'},
+  {no:'0000000167',nama:'ADAM',hp:'-'},
+  {no:'0000000170',nama:'GINO',hp:'0895330957717'},
+  {no:'0000000171',nama:'NGADIMAN',hp:'083825410163'},
+  {no:'0000000172',nama:'PAIMAN',hp:'082123210944'},
+  {no:'0000000173',nama:'POLMA LAY',hp:'085715325616'},
+  {no:'0000000174',nama:'NINING R',hp:'089603025585'},
+  {no:'0000000179',nama:'JONI P',hp:'08811499313'},
+  {no:'0000000180',nama:'FERY KIMBOK',hp:'082310921469'},
+  {no:'0000000181',nama:'SULISTIONO / TIO',hp:'081906126161'},
+  {no:'0000000182',nama:'AHMAD JUNAIDI',hp:'085863747798'},
+  {no:'0000000183',nama:'INDRA / NDOY',hp:'-'},
+  {no:'0000000186',nama:'SUHERMAN',hp:'085710485539'},
+  {no:'0000000187',nama:'SURYATNA',hp:'081219655006'},
+  {no:'0000000188',nama:'ALFAMART',hp:'-'},
+  {no:'0000000189',nama:'AGEN TELOR',hp:'-'},
+  {no:'0000000190',nama:'PADEL',hp:'-'},
+  {no:'0000000192',nama:'MASJID ACEH',hp:'-'},
+  {no:'0000000246',nama:'ARI',hp:'08138091963'},
+  {no:'0000522147',nama:'MARDIYANTO',hp:'085281901019'},
+  {no:'0004921430',nama:'INDRA / NDOYY',hp:'-'},
+  {no:'0004925904',nama:'INGGA G',hp:'-'},
+  {no:'0004931465',nama:'ARIA WISNU',hp:'089669330944'},
+  {no:'0004932129',nama:'CHIRIL',hp:'085780758887'},
+  {no:'0004932162',nama:'MAULID',hp:'085236919992'},
+  {no:'0004934115',nama:'DAVID',hp:'087713357485'},
+  {no:'0004935158',nama:'DIVA',hp:'-'},
+  {no:'0004935216',nama:'ASMODIN',hp:'0838909000367'},
+  {no:'0004935992',nama:'FAWZI / OJI',hp:'089603025585'},
+  {no:'0004937836',nama:'IWAN KURNIAWAN',hp:'08996245759'},
+  {no:'0004940946',nama:'CICE NIEN O',hp:'0819436444123'},
+];
+const WARGA3 = [
+  {no:'0004946524',nama:'UMI / TONI',hp:'085812841606'},
+  {no:'0004946781',nama:'LIVANDRY S',hp:'08298090468'},
+  {no:'0004949455',nama:'OVIEN',hp:'082221047646'},
+  {no:'0004950697',nama:'ANDRI W',hp:'-'},
+  {no:'0004956066',nama:'IKHSANUL PUTRA',hp:'082123728260'},
+  {no:'0004957598',nama:'LANNI HB',hp:'082287154162'},
+  {no:'0004961281',nama:'SRI ATUN',hp:'-'},
+  {no:'0004961639',nama:'SUHANDA',hp:'085717680174'},
+  {no:'0004964948',nama:'ROHIM',hp:'085782602638'},
+  {no:'0004966614',nama:'INDAH PERMATA S',hp:'082373345955'},
+  {no:'0004968021',nama:'SANTI W',hp:'085246652132'},
+  {no:'0004968068',nama:'AGUNG DEMA MARHENTO',hp:'085921211033'},
+  {no:'0004969208',nama:'ARYO',hp:'085717097055'},
+  {no:'0004969299',nama:'FRANSISCA W',hp:'08119105315'},
+  {no:'0004970728',nama:'ARIF',hp:'08129343147'},
+  {no:'0004971676',nama:'SUGENG',hp:'-'},
+  {no:'0004976144',nama:'ANDI WICAKSANA',hp:'087822660286'},
+  {no:'0004976340',nama:'DENU ATMAJA',hp:'081212169993'},
+  {no:'0004983558',nama:'SUKANDI',hp:'082112015392'},
+  {no:'0004987091',nama:'NABILLA',hp:'0857019123'},
+  {no:'0004988561',nama:'ECKY',hp:'087733986978'},
+  {no:'0004989498',nama:'H YANTO',hp:'08161653797'},
+  {no:'0004990017',nama:'KIM',hp:'085607622977'},
+  {no:'0004990793',nama:'BAMBANG',hp:'-'},
+  {no:'0004992670',nama:'AINA URBA',hp:'082287154162'},
+  {no:'0004997694',nama:'YUNI',hp:'-'},
+  {no:'0004998010',nama:'ROSIANTI HADI P',hp:'085810600250'},
+  {no:'0004998593',nama:'OKKY',hp:'085695238031'},
+  {no:'0004999756',nama:'DEWINTA',hp:'085313344726'},
+  {no:'0005001836',nama:'BAYU-FITRI',hp:'081818822793'},
+  {no:'0005002262',nama:'SAHUD - MAK ONI',hp:'089653831507'},
+  {no:'0005002813',nama:'JOEY',hp:'081357352701'},
+  {no:'0005003698',nama:'M FADLI',hp:'081779950556'},
+  {no:'0005004169',nama:'LIKHUN',hp:'087705016190'},
+  {no:'0005005417',nama:'NAFIZHA',hp:'-'},
+  {no:'0005017619',nama:'WILMA',hp:'087884126462'},
+  {no:'0005017784',nama:'CHRISTOPER L',hp:'081375332643'},
+  {no:'0005024944',nama:'BAIM',hp:'089513899071'},
+  {no:'0005025232',nama:'YOLA',hp:'081214134248'},
+  {no:'0005028930',nama:'ERIKA',hp:'KONTRAK - HJ UTI'},
+  {no:'0005029437',nama:'ALI YUNUS',hp:'085281986904'},
+  {no:'0005033553',nama:'IIS',hp:'083870485685'},
+  {no:'0005033903',nama:'NYOMAN',hp:'085945530535'},
+  {no:'0005034117',nama:'DAVID PRIYADI',hp:'085794264211'},
+  {no:'0005034911',nama:'AWI-UNYIL',hp:'081210094122'},
+  {no:'0005036825',nama:'ADI',hp:'-'},
+  {no:'0005037578',nama:'PURWONO',hp:'0817599324'},
+  {no:'0005039437',nama:'SUNARDIYANTO / GENDUT',hp:'085281901019'},
+  {no:'0005040957',nama:'WAWAN GUNAWAN',hp:'081399733870'},
+  {no:'0005048103',nama:'NURHAYATI',hp:'081370658092'},
+];
+const WARGA4 = [
+  {no:'0005055239',nama:'FADZILAH',hp:'082234759171'},
+  {no:'0005057017',nama:'WINIHARSO',hp:'085249557002'},
+  {no:'0005059242',nama:'AGUS NAIM',hp:'08565577552'},
+  {no:'0005062539',nama:'AKO',hp:'082249921741'},
+  {no:'0005063425',nama:'NADIAFSYA',hp:'088232015229'},
+  {no:'0005066894',nama:'ASRHON',hp:'089504614780'},
+  {no:'0005069973',nama:'RUSTONO',hp:'081385513049'},
+  {no:'0005072777',nama:'ISMAIL SUBANA',hp:'083878585837'},
+  {no:'0005074213',nama:'ADI SUSANTO',hp:'085695063489'},
+  {no:'0005075602',nama:'ALI YUSUF',hp:'-'},
+  {no:'0005077135',nama:'ARAIYAH',hp:'081355003410'},
+  {no:'0005077728',nama:'DHANI',hp:'085697891135'},
+  {no:'0005079770',nama:'KARSIM',hp:'085697954574'},
+  {no:'0005081231',nama:'ANDRE',hp:'0812172888016'},
+  {no:'0005087205',nama:'RAHMAT',hp:'-'},
+  {no:'0005088498',nama:'H SOSIAWAN',hp:'-'},
+  {no:'0005089073',nama:'ZALDY',hp:'082114704545'},
+  {no:'0005089412',nama:'DAMBAS',hp:'081517892573'},
+  {no:'0005092427',nama:'ANDRI',hp:'-'},
+  {no:'0005093609',nama:'DIAH WAHYU F',hp:'085156377541'},
+  {no:'0005099616',nama:'CHANDRA',hp:'081515669493'},
+  {no:'0005100846',nama:'KHUSEN',hp:'-'},
+  {no:'0005103627',nama:'SAPTONO',hp:'081318938658'},
+  {no:'0005199987',nama:'LIDYA F',hp:'081280159983'},
+  {no:'0005200456',nama:'ASRON',hp:'081361669666'},
+  {no:'0005202152',nama:'BASROWI',hp:'088211472198'},
+  {no:'0005202827',nama:'IYAN - KEBAB',hp:'081213160598'},
+  {no:'0005202956',nama:'SARBINI',hp:'085881793115'},
+  {no:'0005205587',nama:'ANDREW KS',hp:'082273083446'},
+  {no:'0005209282',nama:'SUPRIYONO',hp:'082211015506'},
+  {no:'0005212014',nama:'DIANA',hp:'085718083974'},
+  {no:'0005212276',nama:'SHOLEHATI',hp:'0822111081657'},
+  {no:'0005214913',nama:'CHOIRUNISA',hp:'085266666505'},
+  {no:'0005217028',nama:'MERY / NASRULLAH',hp:'088975921141'},
+  {no:'0005219903',nama:'REZA RAHMA A',hp:'081287731502'},
+  {no:'0005220633',nama:'ANDRI',hp:'08561687874'},
+  {no:'0005225013',nama:'AZAM',hp:'081384153203'},
+  {no:'0005225721',nama:'SAFEI',hp:'085692014978'},
+  {no:'0005226049',nama:'ERVANSYAH',hp:'0812888844'},
+  {no:'0005228535',nama:'ENI MARITA S',hp:'085276056514'},
+  {no:'0005229651',nama:'M RIZKY',hp:'-'},
+  {no:'0005230509',nama:'ROY',hp:'08561540202'},
+  {no:'0005241192',nama:'GUNAWAN',hp:'081280189199'},
+  {no:'0005241437',nama:'LUKMAN',hp:'089635719264'},
+  {no:'0005244287',nama:'EKO',hp:'081290820312'},
+  {no:'0005245246',nama:'KARWITA',hp:'-'},
+  {no:'0005245287',nama:'YANTO',hp:'085708727659'},
+  {no:'0005247337',nama:'RICKY',hp:'081290595812'},
+  {no:'0005249182',nama:'YANDRA',hp:'081380661791'},
+  {no:'0005249656',nama:'SYUKHENDRI / KEN',hp:'081398697848'},
+];
+const WARGA5 = [
+  {no:'0005251659',nama:'ANWAR',hp:'085883764897'},
+  {no:'0005256000',nama:'YULIA',hp:'0895376710785'},
+  {no:'0005271690',nama:'DENI',hp:'085709305915'},
+  {no:'0005276378',nama:'NURINDAH',hp:'082130860505'},
+  {no:'0005276500',nama:'SUPRAPTO',hp:'081318802245'},
+  {no:'0005277304',nama:'SUYATNO',hp:'08176552009'},
+  {no:'0005278853',nama:'MANIS M',hp:'0887433063411'},
+  {no:'0005283014',nama:'RENA SANJAYA',hp:'081290340236'},
+  {no:'0005285221',nama:'ALEX',hp:'081510924462'},
+  {no:'0005285998',nama:'RIS SUDRAJAT',hp:'081296372096'},
+  {no:'0005287373',nama:'WAHYU SRI - ALM. UJO',hp:'081217976033'},
+  {no:'0005291576',nama:'MOH IDHAM',hp:'081287044231'},
+  {no:'0005303971',nama:'ZAINUDIN',hp:'085691300893'},
+  {no:'0005307844',nama:'GUSMAN URIP',hp:'085710772504'},
+  {no:'0005309165',nama:'NOVI AZIS',hp:'085692347458'},
+  {no:'0005316017',nama:'ARYA',hp:'081345241482'},
+  {no:'0005337436',nama:'GUNUNG ISWAHYUDI',hp:'082114777892'},
+  {no:'0005340151',nama:'KEBAB / IAN',hp:'-'},
+  {no:'0005360954',nama:'KARDI',hp:'087822243515'},
+  {no:'0005369731',nama:'AMAR',hp:'081213567147'},
+  {no:'0005431628',nama:'WAFA',hp:'085858170203'},
+  {no:'0005434174',nama:'DEDY NUR',hp:'08999997092'},
+  {no:'0005460037',nama:'TURUT',hp:'085710476147'},
+  {no:'0005524396',nama:'JODHI P',hp:'085697791189'},
+  {no:'0005551748',nama:'BEBEN',hp:'081290852256'},
+  {no:'0005649390',nama:'YUSUF',hp:'KONTRAK - HJ UTI'},
+  {no:'0006037427',nama:'ANGGI',hp:'085174097474'},
+  {no:'0006042743',nama:"MA'RUF",hp:'089513899071'},
+  {no:'0006044884',nama:'IRINA',hp:'085960270390'},
+  {no:'0006045427',nama:'RANGGA',hp:'082374904196'},
+  {no:'0006048796',nama:'SRI-WANTO',hp:'081316413560'},
+  {no:'0006050900',nama:'ALVIN',hp:'081361985070'},
+  {no:'0006060022',nama:'ARTHA',hp:'089601711149'},
+  {no:'0006060363',nama:'SHALI',hp:'0813382325529'},
+  {no:'0006074245',nama:'SUTRISNA',hp:'-'},
+  {no:'0006077888',nama:'DEA',hp:'082272476395'},
+  {no:'0006081313',nama:'BP ADIT WARKOP',hp:'-'},
+  {no:'0006084744',nama:'FURQON',hp:'083812922913'},
+  {no:'0006088629',nama:'REZA',hp:'087882321722'},
+  {no:'0006088891',nama:'DWI M',hp:'081388968519'},
+  {no:'0006091341',nama:'WAHADI',hp:'085893576614'},
+  {no:'0006092043',nama:'OCA',hp:'-'},
+  {no:'0006094203',nama:'RISMA JARKASI',hp:'08174900107'},
+  {no:'0006098261',nama:'MESIASH',hp:'085975444349'},
+  {no:'0006101204',nama:'ANDRIYANSAH',hp:'089602814473'},
+  {no:'0006102662',nama:'RAFLI YOLANDA',hp:'081214134243'},
+  {no:'0006106774',nama:'DEDY',hp:'081281818056'},
+  {no:'0006109093',nama:'OKTARIA M',hp:'081289023772'},
+  {no:'0006111030',nama:'AVININDA',hp:'085914062743'},
+  {no:'0006114965',nama:'RIZKY C',hp:'-'},
+  {no:'0006115130',nama:'HERA S',hp:'085959466016'},
+  {no:'0006123136',nama:'YUNI',hp:'081381249809'},
+  {no:'0006125818',nama:'SULISTIONO',hp:'081906126161'},
+  {no:'0006127061',nama:'GIANDRA',hp:'083168026208'},
+  {no:'0012477582',nama:'BEBEK',hp:'-'},
+  {no:'0049622738',nama:'REGITA W',hp:'082225618949'},
+];
+
+// ===== GABUNGKAN SEMUA DATA =====
+const ALL_WARGA = [...WARGA,...WARGA2,...WARGA3,...WARGA4,...WARGA5];
+
+// ===== RENDER TABEL IURAN =====
+(function renderIuran() {
+  const tbody = document.getElementById('tbodyIuran');
+  if (!tbody) return;
+
+  // Baca status dari localStorage jika ada (diset oleh admin)
+  let saved = {};
+  try {
+    const d = JSON.parse(localStorage.getItem('rt04_iuran')) || [];
+    d.forEach(x => { saved[x.noRumah] = { status: x.status || 'Belum Bayar', tglBayar: x.tglBayar || '' }; });
+  } catch(e) {}
+
+  const fmtDate = d => {
+    if (!d) return '-';
+    const p = d.split('-');
+    const bln = ['','Jan','Feb','Mar','Apr','Mei','Jun','Jul','Agu','Sep','Okt','Nov','Des'];
+    return (+p[2]) + ' ' + (bln[+p[1]] || '') + ' ' + p[0];
+  };
+
+  let jmlLunas = 0, totalTerkumpul = 0;
+
+  tbody.innerHTML = ALL_WARGA.map((w, i) => {
+    const info    = saved[w.no] || { status: 'Belum Bayar', tglBayar: '' };
+    const isLunas = info.status === 'Lunas';
+    if (isLunas) { jmlLunas++; totalTerkumpul += 50000; }
+    const badge = isLunas
+      ? '<span class="status-lunas">Lunas</span>'
+      : '<span class="status-belum">Belum Bayar</span>';
+    return `<tr>
+      <td>${i + 1}</td>
+      <td style="font-size:.82rem">${w.no}</td>
+      <td>${w.nama}</td>
+      <td style="font-size:.82rem;color:#666">${w.hp}</td>
+      <td>Rp 30.000</td>
+      <td>Rp 20.000</td>
+      <td><strong>Rp 50.000</strong></td>
+      <td>${badge}</td>
+      <td>${fmtDate(info.tglBayar)}</td>
+    </tr>`;
+  }).join('');
+
+  // Update stat cards
+  const jmlBelum = ALL_WARGA.length - jmlLunas;
+  const fmtRp = n => 'Rp ' + n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  const elL = document.getElementById('statLunas');
+  const elB = document.getElementById('statBelum');
+  const elT = document.getElementById('statTotal');
+  if (elL) elL.textContent = jmlLunas;
+  if (elB) elB.textContent = jmlBelum;
+  if (elT) elT.textContent = fmtRp(totalTerkumpul);
+})();
+
+// ===== FILTER TABEL =====
+function filterTable() {
+  const cari   = (document.getElementById('searchWarga')?.value || '').toLowerCase();
+  const status = (document.getElementById('filterStatus')?.value || '');
+  document.querySelectorAll('#iuranTable tbody tr').forEach(row => {
+    const no    = row.cells[1]?.textContent.toLowerCase() || '';
+    const nama  = row.cells[2]?.textContent.toLowerCase() || '';
+    const st    = row.cells[7]?.textContent.trim() || '';
+    const okS   = !cari   || no.includes(cari) || nama.includes(cari);
+    const okSt  = !status || st === status;
+    row.style.display = okS && okSt ? '' : 'none';
+  });
+}
